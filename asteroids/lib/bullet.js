91580ab707b516b83295391bdeecc9af37b46a8e
Christopher Huang <CCHuang90@gmail.com>
2015-09-28 22:12:48 -0700

    rearranged files

--- asteroids/lib/bullet.js
+++ asteroids/lib/bullet.js
@@ -6,6 +6,7 @@ var A = window.Asteroids;
 
 A.Bullet = function (ship) {
   this.pos = ship.pos.slice();
+  this.start_pos = ship.pos.slice();
   this.aim = ship.orientation;
   this.vel = [5*Math.cos(this.aim) + 1.5 * ship.vel[0],
     -5 * Math.sin(this.aim) + 1.5 * ship.vel[1]];
